ec488e14dc8ef803ce1cb0c67acdad0b8e726cd1
Jimmy <dm.a.uvarov@gmail.com>
2022-03-25 15:00:43 -0400

    added user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
   
   resources :posts, only: [:create, :update, :destory, :index, :show]
-  resources :users
+  #resources :users
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  # routes for login logout and so
+  #signup --> users#signup
+  post "/signup",   to: "users#signup"
+  # shows current user
+  get "/me",        to: "users#show"
+
+  #login in
+  post "/login",    to:"sessions#login"
+  #logout
+  delete "/logout",to:"sessions#logout"
+
+
+
+
 end
