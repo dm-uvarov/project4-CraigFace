ac57029de59d577b29701f16c313c276e5de205f
Matt Quinn <mjq0533@gmail.com>
2022-03-25 14:59:29 -0500

    form progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
+import NewPost from './NewPost';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch("/posts")
+      .then(r=>r.json())
+      .then(data => setPosts(data))
+  }, [])
+
+
+  const createPost = (p) => {
+    fetch('/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(p)
+    })
+      .then(r=>r.json())
+      .then(newObj => {
+        setPosts([...posts, newObj])
+      })
+  }
+
+ 
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route>
+          <NewPost createPost={createPost} />
+        </Route>
+      </Switch>
+      
+      
     </div>
   );
 }
